4f3175a259339e1e880f54fe54d4bcbd2f310f58
Suhail Idrees <gusmanbhai@hotmail.com>
2016-12-07 21:00:16 -0800

    Connect to mongodb using the mongoose ORM

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,50 @@
 var express        = require('express');
 var mongoose       = require('mongoose');
-var backController = require('./backend-controller.js')
+//var backController = require('./backend-controller.js')
 var app            = express();
 
-mongoose.connect('mongodb://localhost: 27017/tetris', function(err, db) {
-	if(!err) {
-		console.log('Connected to Mongodb!')
-	}
+mongoose.connect('mongodb://localhost:27017/tetris');
+
+// Connect to database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+app.get('/', function(req,res){
+	hello
 })
 
-app.use(express.static(__dirname + '/../client'));
+// Define userSchema
+
+
+var userSchema = mongoose.Schema({
+  name: String, 
+  score: Number
+});
+
+// Create model
+var User = mongoose.model("User", userSchema);
+
+var max = new User({name: 'Max', score: 999});
+
+//save user to mongodb
+max.save(function (err, max) {
+	if(err) return console.error(err);
+})
+
+User.find(function(err, users) {
+	if(err) return console.error(err);
+})
+console.log(max.name,"******")
+console.log(max.score,"$$$$$$$")
+//app.use(express.static(__dirname + '/../client'));
 
 app.get('/users', function(req, res) {
-	db.userdata.find();
-	res.send('hello world')
+	User.findOne({'name': 'Max'}, function(err, users) {
+		res.send(users)
+	})
 })
 
 app.listen(3000, function() {
